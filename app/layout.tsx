aa3dcc03dee57cf56691d7b1ed0aa30960661877
yassernet1990 <yasser.sr1990@gmail.com>
2026-09-09 17:46:49 +0300

    Load MASAR V19 hero spacing refinements

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import "./masar-v15.css";
 import "./masar-v16.css";
 import "./masar-v17.css";
 import "./masar-v18.css";
+import "./masar-v19.css";
 import ThemeBridge from "./theme-bridge";
 import MasarV17Bridge from "./masar-v17-bridge";
 
