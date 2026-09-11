cf55aaa2b1f64e5ffa440f237eeb2e0bc6ed6a99
yassernet1990 <yasser.sr1990@gmail.com>
2026-09-11 06:26:43 +0300

    Load executive polish layer for Yasser page

--- app/yasser/layout.tsx
+++ app/yasser/layout.tsx
@@ -1,5 +1,6 @@
 import "./polish.css";
 import "./hero-fix.css";
+import "./executive-polish.css";
 
 export default function YasserLayout({ children }: { children: React.ReactNode }) {
   return children;
