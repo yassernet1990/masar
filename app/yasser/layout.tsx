34e00dedfa9b9e05b37f9af9d3771b3aaba4fcc7
yassernet1990 <yasser.sr1990@gmail.com>
2026-09-11 10:55:28 +0300

    Load Yasser partnership copy overrides

--- app/yasser/layout.tsx
+++ app/yasser/layout.tsx
@@ -1,6 +1,7 @@
 import "./section-rhythm.css";
 import "./icons.css";
 import "./sections-23-premium.css";
+import "./partner-copy.css";
 import "./footer-match.css";
 
 export default function YasserLayout({ children }: { children: React.ReactNode }) {
