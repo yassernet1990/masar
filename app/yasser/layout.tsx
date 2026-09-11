7310f4921e8e451f7340f3480060d1fdd4a84046
yassernet1990 <yasser.sr1990@gmail.com>
2026-09-11 12:05:54 +0300

    Load founder typography refinement

--- app/yasser/layout.tsx
+++ app/yasser/layout.tsx
@@ -3,6 +3,7 @@ import "./icons.css";
 import "./sections-23-premium.css";
 import "./partner-copy.css";
 import "./founder-metrics.css";
+import "./founder-type-tweak.css";
 import "./footer-match.css";
 
 export default function YasserLayout({ children }: { children: React.ReactNode }) {
