b0e1e8c0c9b47cb0ca19d0b7abd4a5dc5d4d977a
yassernet1990 <yasser.sr1990@gmail.com>
2026-09-11 11:57:25 +0300

    Load isolated founder metrics styles

--- app/yasser/layout.tsx
+++ app/yasser/layout.tsx
@@ -2,6 +2,7 @@ import "./section-rhythm.css";
 import "./icons.css";
 import "./sections-23-premium.css";
 import "./partner-copy.css";
+import "./founder-metrics.css";
 import "./footer-match.css";
 
 export default function YasserLayout({ children }: { children: React.ReactNode }) {
